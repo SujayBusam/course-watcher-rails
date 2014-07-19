a3270625f8b5f310d3c4398224bd32f6018371cb
Sujay Busam <sujay.busam@gmail.com>
2014-07-19 17:11:21 -0400

    Attempt to fix clock process

--- lib/clock.rb
+++ lib/clock.rb
@@ -17,7 +17,7 @@ include Clockwork
 heroku = Heroku::API.new(api_key: '764626c9-b48d-43a2-a71f-4b060c88fd13')
  
 every(20.seconds, 'Update Attributes') do 
-  heroku.post_ps_scale("coursewatch", "clock", 1)
+  heroku.post_ps_scale("coursewatch", "clock", "1")
   Course.update_attributes
-  heroku.post_ps_scale("coursewatch", "clock", 0)
+  heroku.post_ps_scale("coursewatch", "clock", "0")
 end
